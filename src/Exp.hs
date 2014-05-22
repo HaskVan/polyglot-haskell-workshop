22a9e67aa96fceda1d1b490656292d8cb1e08fb5
Roman Gonzalez <romanandreg@gmail.com>
2014-05-22 16:06:52 -0700

    Add Aeson instances to Exp

--- src/Exp.hs
+++ src/Exp.hs
@@ -1,10 +1,18 @@
 module Exp where
 
+import Data.Aeson (FromJSON(..), ToJSON(..))
+
 data Exp
   = Sum Exp Exp
   | Prod Exp Exp
   | Number Int
   deriving (Show, Eq, Ord)
+
+instance ToJSON Exp where
+  toJSON = undefined
+
+instance FromJSON Exp where
+  parseJSON = undefined
 
 eval :: Exp -> Int
 eval (Number n) = n
