2d7dbba44d667c7a1d02f4216e21cf0e8c9fd98c
Roman Gonzalez <romanandreg@gmail.com>
2014-05-22 17:07:15 -0700

    Exp Aeson implementation

--- src/Exp/JSON.hs
+++ src/Exp/JSON.hs
@@ -14,7 +14,36 @@ import Parser (parseExp)
 import Types
 
 instance ToJSON Exp where
-  toJSON = undefined
+  toJSON (Sum exp1 exp2) =
+    Aeson.object [ "type" .= ("sum" :: Text)
+                 , "a"    .= exp1
+                 , "b"    .= exp2 ]
+
+  toJSON (Prod exp1 exp2) =
+    Aeson.object [ "type" .= ("prod" :: Text)
+                 , "a"    .= exp1
+                 , "b"    .= exp2 ]
+
+  toJSON (Number n) =
+    Aeson.object [ "type"  .= ("number" :: Text)
+                 , "value" .=  n ]
 
 instance FromJSON Exp where
-  parseJSON = undefined
+  parseJSON value@(Aeson.Object obj) = do
+    expType <- obj .: "type"
+    case expType of
+      Aeson.String "prod"   -> Prod   <$> obj .: "a" <*> obj .: "b"
+      Aeson.String "sum"    -> Sum    <$> obj .: "a" <*> obj .: "b"
+      Aeson.String "number" -> Number <$> obj .: "value"
+      _                     -> typeMismatch "Exp" value
+
+  parseJSON value@(Aeson.Number n) =
+    case floatingOrInteger n of
+      Right integer -> return $ Number integer
+      Left _ -> typeMismatch "Exp.Number" value
+
+  parseJSON value@(Aeson.String s) =
+    case parseExp s of
+      Right result -> return result
+      Left  err    -> typeMismatch ("Exp (" ++ show err ++ ")") value
+  parseJSON value = typeMismatch "Exp" value
